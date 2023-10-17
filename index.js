4263d4682209bee7af36677757b4913cfbffad00
allyson_s <allysonsmithstudio@gmail.com>
2023-10-17 11:06:58 -0400

    Desktop styles and mobile menu

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// javascript
+//mobile menu
+const menuBtn = document.querySelector(".menu-btn");
+const exitBtn = document.querySelector(".exit-btn");
+const menu = document.querySelector(".nav-menu");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.transform = "translateX(0%)";
+});
+
+exitBtn.addEventListener("click", () => {
+  menu.style.transform = "translateX(100%)";
+});
